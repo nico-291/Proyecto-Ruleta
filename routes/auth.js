fd0c4a3f0d15c33ca1fed552037d98c48bdf6802
Nicolas Hermosilla <nicolas.hermosilla1@mail.udp.cl>
2025-12-10 23:40:43 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// POST /api/register
+
 router.post('/register', async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
     
-    // Validaciones básicas
     if (!username || !password || !email) {
         return res.status(400).json({ success: false, error: 'Faltan datos' });
     }
@@ -20,7 +19,6 @@ router.post('/register', async (req, res) => {
     const newUser = new User({ nombre: name, email, username, password });
     await newUser.save();
 
-    // Crear Cookie manual (como lo tenías)
     res.cookie('userId', newUser._id.toString(), { httpOnly: true, maxAge: 7 * 24 * 3600000 });
 
     res.json({ success: true, redirectUrl: '/perfil' });
@@ -30,8 +28,6 @@ router.post('/register', async (req, res) => {
     res.status(500).json({ success: false, error: 'Error del servidor' });
   }
 });
-
-// POST /api/login
 router.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -41,8 +37,6 @@ router.post('/login', async (req, res) => {
 
     const isMatch = await user.comparePassword(password);
     if (!isMatch) return res.status(400).json({ success: false, error: 'Credenciales inválidas' });
-
-    // Crear Cookie
     res.cookie('userId', user._id.toString(), { httpOnly: true, maxAge: 7 * 24 * 3600000 });
 
     res.json({ success: true, redirectUrl: '/perfil' });
